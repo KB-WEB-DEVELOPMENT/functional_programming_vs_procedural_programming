8fe680aa869b7744ae128999d5f94d74b3a398ce
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-07-20 14:25:28 +0200

    Add files via upload

---
+++ 12. Transforming Form Input/declarative/funcs.php
@@ -0,0 +1,10 @@
+<?php
+
+function getData() {
+
+  return [
+          ['Jane', 'Bob', 'Mary'],
+          ['jane@example.com', 'bob@example.com', 'mary@example.com'],
+          ['Doctor', 'Plumber', 'Dentist'],
+  ];
+}
