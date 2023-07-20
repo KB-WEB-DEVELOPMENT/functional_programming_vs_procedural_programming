f654003dfd71de592459981faa465a4964b5fa1f
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-07-20 14:16:56 +0200

    Add files via upload

---
+++ 9. Nitpicking a Pull Request/imperative/pullRequests.php
@@ -0,0 +1,504 @@
+<?php
+  return [
+            [
+              'filename' => 'file1.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file2.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other pull request comment',
+            ],
+            [
+              'filename' => 'file3.ph',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file4.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file5.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'still some other different comment',
+            ],
+            [
+              'filename' => 'file6.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  'still some other different comment',
+            ],
+            [
+              'filename' => 'file7.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file8.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file9.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other pull request comment',
+            ],
+            [
+              'filename' => 'file10.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file11.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file12.ph',
+              'fileStatus' => 'changed',
+              'comments'  => 'still some other different comment',
+            ],
+            [
+              'filename' => 'file13.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  'still some other different comment',
+            ],
+            [
+              'filename' => 'file14.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file15.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file16.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other pull request comment',
+            ],
+            [
+              'filename' => 'file17.ph',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file18.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file19.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'still some other different comment',
+            ],
+            [
+              'filename' => 'file20.p?p',
+              'fileStatus' => 'changed',
+              'comments'  =>  'still some other different comment',
+            ],
+            [
+              'filename' => 'file21.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file22.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file23.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file24.ph',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file25.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file26.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file27.p?p',
+              'fileStatus' => 'changed',
+              'comments'  =>  'still some other different comment',
+            ],
+            [
+              'filename' => 'file28.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file29.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file30.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other pull request comment',
+            ],
+            [
+              'filename' => 'file31.ph',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file32.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file33.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'still some other different comment',
+            ],
+            [
+              'filename' => 'file34.p?p',
+              'fileStatus' => 'changed',
+              'comments'  =>  'some other again pull request commentt',
+            ],
+            [
+              'filename' => 'file35.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file36.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file37.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other pull request comment',
+            ],
+            [
+              'filename' => 'file38.ph',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file39.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file40.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'still some other different comment',
+            ],
+            [
+              'filename' => 'file41.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  'still some other different comment',
+            ],
+            [
+              'filename' => 'file42.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file43.p?p',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file44.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file45.ph',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file46.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file47.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'still some other different comment',
+            ],
+            [
+              'filename' => 'file48.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file49.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file50.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file51.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other pull request comment',
+            ],
+            [
+              'filename' => 'file52.ph',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file53.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file54.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'still some other different comment',
+            ],
+            [
+              'filename' => 'file55.p?p',
+              'fileStatus' => 'changed',
+              'comments'  =>  'still some other different comment',
+            ],
+            [
+              'filename' => 'file56.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file57.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file58.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other pull request comment',
+            ],
+            [
+              'filename' => 'file59.ph',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file60.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file61.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'still some other different comment',
+            ],
+            [
+              'filename' => 'file62.p?p',
+              'fileStatus' => 'changed',
+              'comments'  =>  'still some other different comment',
+            ],
+            [
+              'filename' => 'file63.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file64.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file65.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other pull request comment',
+            ],
+            [
+              'filename' => 'file66.ph',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file67.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file68.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'still some other different comment',
+            ],
+            [
+              'filename' => 'file69.p?p',
+              'fileStatus' => 'changed',
+              'comments'  =>  'still some other different comment',
+            ],
+            [
+              'filename' => 'file70.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file71.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file72.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other pull request comment',
+            ],
+            [
+              'filename' => 'file73.ph',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file74.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file75.p?p',
+              'fileStatus' => 'changed',
+              'comments'  => 'still some other different comment',
+            ],
+            [
+              'filename' => 'file76.php',
+              'fileStatus' => 'unchanged',
+              'comments'  =>  'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file77.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file78.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file79.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other pull request comment',
+            ],
+            [
+              'filename' => 'file80.ph',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file81.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file82.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'still some other different comment',
+            ],
+            [
+              'filename' => 'file83.p?p',
+              'fileStatus' => 'unchanged',
+              'comments'  =>  'still some other different comment',
+            ],
+            [
+              'filename' => 'file84.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file85.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file86.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file87.ph',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file88.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file89.p?p',
+              'fileStatus' => 'changed',
+              'comments'  => 'still some other different comment',
+            ],
+            [
+              'filename' => 'file90.php',
+              'fileStatus' => 'unchanged',
+              'comments'  =>  'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file91.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file92.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some pull request comment',
+            ],
+            [
+              'filename' => 'file93.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other pull request comment',
+            ],
+            [
+              'filename' => 'file94.ph',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file95.php',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other different pull request comment',
+            ],
+            [
+              'filename' => 'file96.p?p',
+              'fileStatus' => 'changed',
+              'comments'  => 'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file97.php',
+              'fileStatus' => 'unchanged',
+              'comments'  =>  'still some other different comment',
+            ],
+            [
+              'filename' => 'file98.php',
+              'fileStatus' => 'changed',
+              'comments'  =>  NULL,
+            ],
+			            [
+              'filename' => 'file99.php',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other again pull request comment',
+            ],
+            [
+              'filename' => 'file100.p?p',
+              'fileStatus' => 'unchanged',
+              'comments'  => 'some other pull request comment',
+            ],
+  ];
+?>
