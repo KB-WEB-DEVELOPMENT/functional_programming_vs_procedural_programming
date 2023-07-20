34a47b779f7fc4878714a72a93c43536e463c505
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-07-20 14:03:35 +0200

    Add files via upload

---
+++ 6. Stealing Mail/imperative/emailMessages.php
@@ -0,0 +1,6 @@
+<?php
+  return [
+	'subject' => 'An example email subject!',
+	'recipients' => ['jane@example.com', 'john@example.com', 'mary@example.com'],
+	'body' => 'An example email body.',
+  ];
