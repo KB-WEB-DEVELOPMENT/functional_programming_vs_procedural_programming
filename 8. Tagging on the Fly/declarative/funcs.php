77a73105e3a10d3495fcaf9f59c2e4ed03dbecbc
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-07-20 14:11:12 +0200

    Add files via upload

---
+++ 8. Tagging on the Fly/declarative/funcs.php
@@ -0,0 +1,16 @@
+<?php
+
+function getTags() {
+
+   return [
+	 17,32,'recipes',11,'kitchen',27,'house',111,'kite',882,
+	 11,58,'taste',27,'bungalow',21,'coca',623,'baloon',914,
+	 19,45,'dessert',74,'outdoors',522,'sprite',5636,'helicopter',714,
+	 28,49,'entrée',26,'indoors',814,'water',269,'field',821,
+	 56,28,'meal',99,'airplane',145,'orange juice',816,'living-room',971,
+	 78,71,'coffee',123,'bycicle',9821,'vodka',592,'toilets',713,
+	 98,65,'tea',471,'airplane',147,'beer',992,'ship',981,
+	 82,53,'milk',7104,'some word',972,'house',281,'mushroom',813,
+	 67,87,'syrup',5721,'some other word',29871,'house',87,'vegetables',98814,
+	 56,825,'wine',3482,'still some other word',2741,'house',87,'meat',148,
+];
