1e1b52ac30de9b6f5e6014aae9f3e7fb6893acc2
Kami Barut-Wanayo <kamibarut@yahoo.com>
2023-07-20 14:32:18 +0200

    Add files via upload

---
+++ 13. Ranking a Competition/imperative/scores.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    ['score' => 76, 'team' => 'A'],['score' => 62, 'team' => 'BA'],['score' => 82, 'team' => 'QE'],['score' => 12, 'team' => 'ER'],['score' => 45, 'team' => 'ERP'],
+    ['score' => 67, 'team' => 'F'],['score' => 67, 'team' => 'GL'],['score' => 67, 'team' => 'DR'],['score' => 28, 'team' => 'QW'],['score' => 23, 'team' => 'SWV'],	
+    ['score' => 76, 'team' => 'K'],['score' => 62, 'team' => 'BX'],['score' => 23, 'team' => 'FO'],['score' => 27, 'team' => 'DL'],['score' => 55, 'team' => 'GOL'],
+    ['score' => 67, 'team' => 'L'],['score' => 43, 'team' => 'PR'],['score' => 87, 'team' => 'NZ'],['score' => 63, 'team' => 'JZ'],['score' => 87, 'team' => 'WLP'],	
+    ['score' => 43, 'team' => 'M'],['score' => 99, 'team' => 'FU'],['score' => 65, 'team' => 'KO'],['score' => 73, 'team' => 'NU'],['score' => 15, 'team' => 'FGO'],
+    ['score' => 12, 'team' => 'O'],['score' => 71, 'team' => 'VW'],['score' => 43, 'team' => 'GH'],['score' => 91, 'team' => 'LO'],['score' => 13, 'team' => 'VLO'],	
+    ['score' => 76, 'team' => 'P'],['score' => 23, 'team' => 'FP'],['score' => 29, 'team' => 'FG'],['score' => 28, 'team' => 'JU'],['score' => 98, 'team' => 'KJR'],
+    ['score' => 67, 'team' => 'Q'],['score' => 76, 'team' => 'HR'],['score' => 81, 'team' => 'LI'],['score' => 48, 'team' => 'NH'],['score' => 12, 'team' => 'DEF'],	
+    ['score' => 51, 'team' => 'R'],['score' => 56, 'team' => 'LO'],['score' => 76, 'team' => 'NZ'],['score' => 27, 'team' => 'BH'],['score' => 58, 'team' => 'GOW'],
+    ['score' => 67, 'team' => 'S'],['score' => 92, 'team' => 'ZU'],['score' => 32, 'team' => 'GR'],['score' => 73, 'team' => 'FPP'],['score' => 26, 'team' => 'WXO'],	
+    ['score' => 55, 'team' => 'T'],['score' => 23, 'team' => 'HK'],['score' => 98, 'team' => 'FO'],['score' => 28, 'team' => 'NBB'],['score' => 24, 'team' => 'QLO'],
+    ['score' => 67, 'team' => 'U'],['score' => 11, 'team' => 'UZ'],['score' => 73, 'team' => 'BV'],['score' => 66, 'team' => 'CVV'],['score' => 87, 'team' => 'GPL'],	
+    ['score' => 99, 'team' => 'V'],['score' => 87, 'team' => 'NU'],['score' => 39, 'team' => 'LP'],['score' => 15, 'team' => 'LOO'],['score' => 17, 'team' => 'FGP'],
+    ['score' => 67, 'team' => 'W'],['score' => 65, 'team' => 'MT'],['score' => 90, 'team' => 'FZ'],['score' => 12, 'team' => 'WSX'],['score' => 87, 'team' => 'LHZ'],	
+    ['score' => 76, 'team' => 'Z'],['score' => 45, 'team' => 'RP'],['score' => 38, 'team' => 'LZ'],['score' => 49, 'team' => 'HGZ'],['score' => 67, 'team' => 'DFP'],
+    ['score' => 76, 'team' => 'AA'],['score' => 29, 'team' => 'ET'],['score' => 76, 'team' => 'QA'],['score' => 23, 'team' => 'JZR'],['score' => 34, 'team' => 'VOU'],	
+    ['score' => 76, 'team' => 'BB'],['score' => 76, 'team' => 'FO'],['score' => 19, 'team' => 'DX'],['score' => 48, 'team' => 'FOI'],['score' => 26, 'team' => 'RPS'],
+    ['score' => 87, 'team' => 'CC'],['score' => 56, 'team' => 'BH'],['score' => 8, 'team' => 'VE'],['score' => 11, 'team' => 'VOZ'],['score' => 29, 'team' => 'DPR'],	
+    ['score' => 67, 'team' => 'EE'],['score' => 43, 'team' => 'FL'],['score' => 6, 'team' => 'TZ'],['score' => 7, 'team' => 'FPO'],['score' => 76, 'team' => 'FGO'],
+    ['score' => 23, 'team' => 'FF'],['score' => 23, 'team' => 'VI'],['score' => 87, 'team' => 'WP'],['score' => 88, 'team' => 'CXP'],['score' => 48, 'team' => 'JJJ'],	
+ 
+ ];
+?>
